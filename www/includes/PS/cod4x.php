efe9eb9be4e2470ec6208a8d4aae589a20732908
Drek <doxology@rogers.com>
2021-06-10 14:44:41 -0300

    Revert team names fix.

--- www/includes/PS/cod4x.php
+++ www/includes/PS/cod4x.php
@@ -34,20 +34,20 @@ class PS_cod4x extends PS {
 var $class = 'PS::cod4x';
 
 var $CLAN_MODTYPES = array(
-	'coalitionkills'		=> '+',
-	'coalitionkillspct'	=> array( 'percent2', 'coalitionkills', 'allegiancekills' ),
-	'allegiancekills'		=> '+',
-	'allegiancekillspct'		=> array( 'percent2', 'allegiancekills', 'coalitionkills' ),
-	'coalitiondeaths'		=> '+',
-	'allegiancedeaths'		=> '+',
-	'joinedcoalition'		=> '+',
-	'joinedallegiance'		=> '+',
-	'coalitionwon'		=> '+',
-	'coalitionwonpct'		=> array( 'percent2', 'coalitionwon', 'allegiancewon' ),
-	'allegiancewon'		=> '+',
-	'allegiancewonpct'		=> array( 'percent2', 'allegiancewon', 'coalitionwon' ),
-	'coalitionlost'		=> '+',
-	'allegiancelost'		=> '+',
+	'allieskills'		=> '+',
+	'allieskillspct'	=> array( 'percent2', 'allieskills', 'axiskills' ),
+	'axiskills'		=> '+',
+	'axiskillspct'		=> array( 'percent2', 'axiskills', 'allieskills' ),
+	'alliesdeaths'		=> '+',
+	'axisdeaths'		=> '+',
+	'joinedallies'		=> '+',
+	'joinedaxis'		=> '+',
+	'allieswon'		=> '+',
+	'allieswonpct'		=> array( 'percent2', 'allieswon', 'axiswon' ),
+	'axiswon'		=> '+',
+	'axiswonpct'		=> array( 'percent2', 'axiswon', 'allieswon' ),
+	'allieslost'		=> '+',
+	'axislost'		=> '+',
 );
 
 
@@ -83,7 +83,7 @@ function maps_table_mod(&$table) {
 	global $cms;
 	$table->insert_columns(
 		array( 
-			'allegiancekillspct' => array( 'label' => $cms->trans('Team Kills'), 'tooltip' => $cms->trans("Allegiance / Coalition Kills"), 'callback' => array(&$this, 'team_wins') ), 
+			'axiskillspct' => array( 'label' => $cms->trans('Team Kills'), 'tooltip' => $cms->trans("Axis / Ally Kills"), 'callback' => array(&$this, 'team_wins') ), 
 		),
 		'rounds',
 		true
@@ -134,22 +134,22 @@ function player_left_column_mod(&$plr, &$theme) {
 	$tpl = 'player_left_column_mod';
 	if ($theme->template_found($tpl, false)) {
 		$actions = array();
-		$joined = $plr['joinedallegiance'] + $plr['joinedcoalition'];
+		$joined = $plr['joinedaxis'] + $plr['joinedallies'];
 		if ($joined) {
-			$pct1 = sprintf('%0.02f', $plr['joinedallegiance'] / $joined * 100);
-			$pct2 = sprintf('%0.02f', $plr['joinedcoalition'] / $joined * 100);
+			$pct1 = sprintf('%0.02f', $plr['joinedaxis'] / $joined * 100);
+			$pct2 = sprintf('%0.02f', $plr['joinedallies'] / $joined * 100);
 		} else {
 			$pct1 = $pct2 = 0;
 		}
 
 		$actions['kills'] = array(
-			'label'	=> $cms->trans("Allegiance / Coalition Kills"),
+			'label'	=> $cms->trans("Axis / Ally Kills"),
 			'type'	=> 'dual_bar',
 			'value'	=> array(
-				'pct1'	 	=> $plr['allegiancekillspct'],
-				'pct2'	 	=> $plr['coalitionkillspct'],
-				'title1'	=> commify($plr['allegiancekills']) . ' ' . $cms->trans('allegiance') . ' (' . $plr['allegiancekillspct'] . '%)',
-				'title2'	=> commify($plr['coalitionkills']) . ' ' . $cms->trans('coalition') . ' (' . $plr['coalitionkillspct'] . '%)',
+				'pct1'	 	=> $plr['axiskillspct'],
+				'pct2'	 	=> $plr['allieskillspct'],
+				'title1'	=> commify($plr['axiskills']) . ' ' . $cms->trans('axis') . ' (' . $plr['axiskillspct'] . '%)',
+				'title2'	=> commify($plr['allieskills']) . ' ' . $cms->trans('allies') . ' (' . $plr['allieskillspct'] . '%)',
 				'width'		=> 130
 			)
 		);
@@ -176,10 +176,10 @@ function player_left_column_mod(&$plr, &$theme) {
 function team_wins($value, $data) {
 	global $cms;
 	$bar = dual_bar(array(
-		'pct1'	=> $data['allegiancekillspct'], 
-		'pct2'	=> $data['coalitionkillspct'],
-		'title1'=> commify($data['allegiancekills']) . " " . $cms->trans("Allegiance Kills") . " (" . $data['allegiancekillspct'] . "%)",
-		'title2'=> commify($data['coalitionkills']) . " " . $cms->trans("Coalition Kills") . " (" . $data['coalitionkillspct'] . "%)",
+		'pct1'	=> $data['axiskillspct'], 
+		'pct2'	=> $data['allieskillspct'],
+		'title1'=> commify($data['axiskills']) . " " . $cms->trans("Axis Kills") . " (" . $data['axiskillspct'] . "%)",
+		'title2'=> commify($data['allieskills']) . " " . $cms->trans("Ally Kills") . " (" . $data['allieskillspct'] . "%)",
 	));
 	return $bar;
 }
